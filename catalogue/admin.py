6af49ddd3527aa09c9ff71834c37f117e40a34cb
misha <misha@mail.com>
2024-04-04 18:28:35 +0300

    Updated DB

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Customer, Order
 # Register your models here.
 admin.site.register(Product)
-
 admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
