f7bf6d4eba3ab0ca4afbe639d8ba4365afc16d05
misha <misha@mail.com>
2024-04-04 18:57:07 +0300

    added product detail page

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,6 +1,7 @@
 from django.urls import  path
-from .views import IndexView
+from .views import IndexView, ProductDetailView
 urlpatterns = [
-    path("", IndexView.as_view(), name="home"),
-    path("featured/", IndexView.as_view(), name="home")
+    path("", IndexView.as_view(template_name="base.html"), name="home"),
+    path("featured/", IndexView.as_view(), name="home"),
+    path("products/<int:pk>", ProductDetailView.as_view(), name="product-detail")
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 # Create your views here.
 class IndexView(ListView):
     model = Product
     template_name = "index.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+
+
